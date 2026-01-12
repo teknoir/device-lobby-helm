b7b3993d9da77f9242a726607439a89dc9bc900a
Anders Åslund <anders.aslund@teknoir.ai>
2026-01-12 15:46:18 +0100

    Bump version to 0.0.1-beta.16 in Chart.yaml and deploy.sh, update PostgreSQL claim name in postgres.yaml

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -97,7 +97,7 @@ metadata:
 spec:
   repo: https://teknoir.github.io/device-lobby-helm
   chart: device-lobby
-  version: 0.0.1-beta.15
+  version: 0.0.1-beta.16
   targetNamespace: ${NAMESPACE}
   valuesContent: |-
     domain: ${DOMAIN}
